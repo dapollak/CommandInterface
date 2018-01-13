14484557398f27e1246edaea020823d3334dab28
Daniel Pollak <>
2018-01-13 18:13:41 +0200

    Rearrange, put all in eponymous template block

--- source.d
+++ source.d
@@ -29,26 +29,30 @@ struct Command {
 	}
 }
 
-void makeCmd(T)(string[] args) {
+template makeCmd(T) {
+
+string genAssigmentString(int numOfParams)() {
+	string res = "";
+	foreach (i; 0..numOfParams) {
+		res ~= "to!(paramTypes["~to!string(i)~"])(args["~to!string(i+2)~"]), ";
+	}
+
+	return res[0..$-2];
+}
+
+void makeCmd(string[] args) {
 	foreach (method; __traits(allMembers, T)) {
 		if (args[1] == method) {
 
 			mixin("enum int numOfParams = Parameters!("~fullyQualifiedName!T~"."~method~").length;");
 			mixin("alias paramTypes = Parameters!("~fullyQualifiedName!T~"."~method~");");
-
-			string genAssigmentString(int numOfParams)() {
-				string res = "";
-				foreach (i; 0..numOfParams) {
-					res ~= "to!(paramTypes["~to!string(i)~"])(args["~to!string(i+2)~"]), ";
-				}
-
-				return res[0..$-2];
-			}
-
+			
 			enum string assignmentString = genAssigmentString!numOfParams();
 			mixin("writeln("~fullyQualifiedName!T~"."~method~"("~assignmentString~"));");
 		}
 	}
+}
+
 }
 
 int main(string[] argv)
